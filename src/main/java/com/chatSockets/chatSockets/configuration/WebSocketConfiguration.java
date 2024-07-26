cba4e67ffb667e95034e17cc38ae73e151358cdb
david <davidhernandez9801@gmail.com>
2024-07-25 23:06:36 -0500

    Update WebSocketConfiguration.java

--- src/main/java/com/chatSockets/chatSockets/configuration/WebSocketConfiguration.java
+++ src/main/java/com/chatSockets/chatSockets/configuration/WebSocketConfiguration.java
@@ -11,7 +11,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry){
-        registry.enableSimpleBroker("/topic"); // Corregido el typo en el nombre del topic
+        registry.enableSimpleBroker("/topic");
         registry.setApplicationDestinationPrefixes("/app");
     }
 
